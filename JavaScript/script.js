5a3961b4c947580fccb878ae5a83f110200db4eb
sabamokhlesi <59446430+sabamokhlesi@users.noreply.github.com>
2020-05-29 15:51:00 -0400

    added intersection animation

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,24 +1,26 @@
-// const MoveInRight = document.querySelectorAll('.move-in-right')
-// const MoveInUp = document.querySelectorAll('.move-in-up')
-// const MoveInLeft = document.querySelectorAll('.move-in-left')
-// const FadeIn = document.querySelectorAll('.fade-in')
-// const options = {
-//     root:null,
-//     threshold:0,
-//     rootMargin:"-200px 0px -200px 0px",
-// }
-// const observer = new IntersectionObserver(function(entries,observer){
-//     entries.forEach(entry =>{
-//         if(!entry.isIntersecting){return}
-//         entry.target.classList.add('add-animation');
-//         observer.unobserve(entry.target)
-//     })
-// },options)
+const MoveInRight = document.querySelectorAll('.move-in-right')
+const MoveInUp = document.querySelectorAll('.move-in-up')
+const MoveInDown = document.querySelectorAll('.move-in-down')
+const MoveInLeft = document.querySelectorAll('.move-in-left')
+const FadeIn = document.querySelectorAll('.fade-in')
+const options = {
+    root:null,
+    threshold:0,
+    rootMargin:"-250px 0px -250px 0px",
+}
+const observer = new IntersectionObserver(function(entries,observer){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){return}
+        entry.target.classList.add('add-animation');
+        observer.unobserve(entry.target)
+    })
+},options)
 
-// MoveInRight.forEach(item => observer.observe(item))
-// MoveInLeft.forEach(item => observer.observe(item))
-// MoveInUp.forEach(item => observer.observe(item))
-// FadeIn.forEach(item => observer.observe(item))
+MoveInRight.forEach(item => observer.observe(item))
+MoveInLeft.forEach(item => observer.observe(item))
+MoveInUp.forEach(item => observer.observe(item))
+MoveInDown.forEach(item => observer.observe(item))
+FadeIn.forEach(item => observer.observe(item))
 
 
 textAnimate();
